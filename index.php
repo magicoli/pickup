28473b8bc5ec77b5e51b61aa15416bc06a1d4341
Olivier van Helden <olivier@van-helden.net>
2020-02-16 14:33:37 -0400

    Added logo select menu

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-$logo="https://lelitdoli.eu/wp-content/uploads/2019/02/lelitdoli-logo-v1-fuchsia-1.png";
+$logos=array(
+  "Le lit d'Oli" => "https://lelitdoli.eu/wp-content/uploads/2019/02/lelitdoli-logo-v1-fuchsia-1.png",
+  "Gîtes Mosaïques" => "https://gites-mosaiques.com/wp-content/uploads/2019/06/mosaiques-logo-v3-1L-yellow.png",
+  "Kaz à Moïse" => "https://kazamoise.fr/wp-content/uploads/2019/03/kazamoise-logo-v0-248x40.png",
+);
+$logo=$_REQUEST[logo];
+if(empty($logo)) $logo=array_values($logos)[0];
+
 $action=$_REQUEST[action];
 if($action != "Clear") {
   $name=$_REQUEST[name];
@@ -31,10 +38,21 @@ $resetlink=$_SERVER['SCRIPT_URI'] . "?" . http_build_query(array_merge( $_GET, a
 </head>
 <body id=panel><center>
   <?php if(empty($name) || $action=="reset") { ?>
-    <div id="logo"><img src="<?php echo $logo; ?>" alt="<?php echo "$company"; ?>"></div>
-    <form>
+    <!-- <div id="logo"><img src="<?php echo $logo; ?>" alt="<?php echo "$company"; ?>"></div> -->
+    <form method="post">
+      <div>
+      <select id="logo" name="logo">
+        echo "<option value=''>-- Select a logo --</option>";
+        <?php while(list($key, $value) = each($logos)) {
+          if($value==$logo) $selected="selected";
+          else $selected="";
+          echo "<option value='$value' $selected>$key</option>";
+        }
+        ?>
+      </select>
+    </div>
       <input type=text name=firstname placeholder="First name" value="<?php echo $firstname ?>">
-      <input type=text name=name placeholder="Last Name (*)" value="<?php echo $name ?>">
+      <input type=text name=name placeholder="Name (required)" value="<?php echo $name ?>">
       <input type=text name=nickname placeholder="Nick name" value="<?php echo $nickname ?>">
       <div>
         <?php if(!empty($firstname . $name . $nickname)) { ?>
@@ -47,7 +65,16 @@ $resetlink=$_SERVER['SCRIPT_URI'] . "?" . http_build_query(array_merge( $_GET, a
   <table>
     <tr>
       <td><div id="header">
-        <a href="<?php echo $resetlink ?>"><div id="logo"><img src="<?php echo $logo; ?>" alt="<?php echo "$company"; ?>"></div></a>
+        <div id="logo">
+          <form method=post>
+            <input type=hidden name=firstname value="<?php echo $firstname ?>">
+            <input type=hidden name=name value="<?php echo $name ?>">
+            <input type=hidden name=nickname value="<?php echo $nickname ?>">
+            <button type="submit" name=action value="reset">
+              <img src="<?php echo $logo; ?>" alt="<?php echo "$company"; ?>">
+            </button>
+          </form>
+        </div>
       </div></td>
     </tr>
     <tr>
