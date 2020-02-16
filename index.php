10726f68d80b34ccd3e9e7395b9f3e4cb3acecce
Olivier van Helden <olivier@van-helden.net>
2020-02-16 13:35:25 -0400

    Added Clear and Reset option Different css for form and display Better display (relative to viewport)

--- index.php
+++ index.php
@@ -1,98 +1,68 @@
 <?php
 $logo="https://lelitdoli.eu/wp-content/uploads/2019/02/lelitdoli-logo-v1-fuchsia-1.png";
-$name=$_REQUEST[name];
-$firstname=$_REQUEST[firstname];
-$nickname=$_REQUEST[nickname];
+$action=$_REQUEST[action];
+if($action != "Clear") {
+  $name=$_REQUEST[name];
+  $firstname=$_REQUEST[firstname];
+  $nickname=$_REQUEST[nickname];
+}
+$resetlink=$_SERVER['SCRIPT_URI'] . "?" . http_build_query(array_merge( $_GET, array( 'action' => 'reset' )));
 ?><html>
 <title><?php echo trim($firstname . " " . $name . " Pick Up"); ?></title>
 <link rel="icon" href="images/pickup-logo-32x32.png" sizes="32x32" />
 <link rel="icon" href="images/pickup-logo-192x192.png" sizes="192x192" />
 <link rel="apple-touch-icon-precomposed" href="images/pickup-logo-180x180.png" />
 
+<link rel='stylesheet' id='pu_main'  href='css/main.css' type='text/css' media='all' />
+<?php if(!empty($_REQUEST[name])) { ?>
+  <link rel='stylesheet' id='pu_main'  href='css/pane.css' type='text/css' media='all' />
+<?php } ?>
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="mobile-web-app-capable" content="yes">
 <link type="text/css" rel="stylesheet[nickname]" href="pub/contrat.css" media="all" />
 <style>
-#panel {
-  font-family: "Helvetica Neue", "Helvetica", Arial, sans-serif;
-  line-height: 100%;
-  margin: 0;
-  padding: 0;
-  width: auto;
-}
-#panel table {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  margin: 0;
-}
-#panel,
-#panel #header,
-#panel #name {
-  text-align: center;
-  font-size: 100pt;
-}
-#panel #header,
-#panel #footer {
-  height: 0px;
-}
 
-#panel #name {
-  vertical-align:middle;
-}
-#panel .firstname {
-  font-size: 70%;
-  font-weight: light;
-  line-height: 100%;
-}
-#panel .lastname {
-  font-weight: bold;
-}
-#panel .nickname {
-  font-size: 50%;
-  line-height: 100%;
-}
-img,
-table,
-tr,
-td,
-div {
-  max-width: 100%;
-}
 </style>
 
 <head>
-  <title><?php echo $name ?></title>
+  <!-- <title><?php echo $name ?></title> -->
 </head>
 <body id=panel><center>
+  <?php if(empty($name) || $action=="reset") { ?>
+    <div id="logo"><img src="<?php echo $logo; ?>" alt="<?php echo "$company"; ?>"></div>
+    <form>
+      <input type=text name=firstname placeholder="First name" value="<?php echo $firstname ?>">
+      <input type=text name=name placeholder="Last Name (*)" value="<?php echo $name ?>">
+      <input type=text name=nickname placeholder="Nick name" value="<?php echo $nickname ?>">
+      <div>
+        <?php if(!empty($firstname . $name . $nickname)) { ?>
+          <input type="submit" name=action value="Clear">
+        <?php } ?>
+        <input type="submit" value="Submit">
+      </div>
+    </form>
+  <?php } else { ?>
   <table>
     <tr>
       <td><div id="header">
-        <div id="logo"><img src="<?php echo $logo; ?>" alt="<?php echo "$company"; ?>"></div>
-      </div>
-    </td>
-  </tr>
-  <tr>
+        <a href="<?php echo $resetlink ?>"><div id="logo"><img src="<?php echo $logo; ?>" alt="<?php echo "$company"; ?>"></div></a>
+      </div></td>
+    </tr>
+    <tr>
     <td id=name valign=middle style="">
-      <?php if(empty($name)) { ?>
-      <h1>Pick Up</h1>
-      <div>Choose a name</div>
-        <form>
-          <input type=text name=firstname placeholder="First name">
-          <input type=text name=name placeholder="Last Name">
-          <!-- <input type=text name=nickname placeholder="Nick name"> -->
-          <input type="submit" value="Submit">
-        </form>
-      <?php } else { ?>
-      <div class=firstname><?php echo $firstname ?></div>
-      <div class=lastname><?php echo $name ?></div>
-    <?php } ?>
+        <?php
+        if(!empty($firstname)) echo "<div class=firstname>$firstname</div>";
+        if(!empty($name)) echo "<div class=lastname>$name</div>";
+        if(!empty($nickname)) echo "<div class=nickname>($nickname)</div>";
+        ?>
     </td></tr>
+      <tr>
+        <td><div id="footer">
+        </div></td>
+      </tr>
   </table>
+  <?php } ?>
 </center></body>
 </html>
