9a27ed0234e74f343e89898c66ff692375a7f933
Olivier van Helden <olivier@van-helden.net>
2020-02-23 09:33:47 -0400

    dowload manager: allow dev or stable; clean up var names

--- download/index.php
+++ download/index.php
@@ -8,8 +8,8 @@ function debug($message) {
 
 $debug="";
 $webroot="";
-$lastversion="";
-$downloadfile="";
+$buildVersion="";
+$buildUrl="";
 $directory=dirname(getenv('SCRIPT_FILENAME'));
 $baseuri=dirname(getenv('SCRIPT_URI')."ignore");
 
@@ -19,23 +19,32 @@ if($d) {
   {
     if (! preg_match("/\.APK/i", $file)) continue;
     $version=(preg_replace("/^[^-]*-([0-9-\.]*)\.apk/i", "\\1", $file));
-    if (version_compare($version, $lastversion, ">")) {
-      $lastversion=$version;
-      $downloadfile="$baseuri/$file";
-      // if(!preg_match("/-/", $version)) {
-      //   $laststable=$lastversion;
-      //   $downloadstable=$downloadfile;
-      // }
+    if (version_compare($version, $buildVersion, ">")) {
+      $buildVersion=$version;
+      $buildUrl="$baseuri/$file";
+      if(!preg_match("/-/", $version)) {
+        $stableVersion=$buildVersion;
+        $stableUrl=$buildUrl;
+      }
     };
   }
 }
+if(isset($_REQUEST['dev'])) {
+  $version=$buildVersion;
+  $location=$buildUrl;
+} else {
+  $version=$stableVersion;
+  $location=$stableUrl;
+}
 if(isset($_REQUEST['query'])) {
   header("Content-Type: text/plain");
-  echo "VERSION=$lastversion\n";
-  echo "LOCATION=$downloadfile\n";
+  header("Access-Control-Allow-Origin: *");
+  echo "VERSION=$buildVersion\n";
+  echo "LOCATION=$buildUrl\n";
   echo $debug;
-} else {
-  header("Location: $downloadfile");
+} else  {
+  header("Location: $location");
   exit();
 }
+
 ?>
