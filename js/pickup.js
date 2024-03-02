91c5dde69fbf87191a6a7221bd70ef36347d534e
Olivier van Helden <olivier@van-helden.net>
2024-03-02 21:15:20 +0100

    added full screen button

--- js/pickup.js
+++ js/pickup.js
@@ -30,6 +30,7 @@ function adjustFontSize() {
     for (var i = 0; i < lines.length; i++) {
       lines[i].style.fontSize = fontSize + 'px';
     }
+  
 }
 
 // Call the adjustFontSize() when the DOM is fully loaded
@@ -99,4 +100,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Le reste de votre code va ici...
   display.addEventListener('click', displayClickListener);
+});
+
+
+// Full screen button
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (document.fullscreenEnabled) {
+    const fullscreen_button = document.createElement("button");
+    fullscreen_button.setAttribute('id','fullscreen-button');
+    fullscreen_button.addEventListener("click", toggle_fullscreen);
+    fullscreen_button.innerHTML  = `
+        <svg viewBox="0 0 24 24">
+            <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 
+            11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>
+        </svg>
+        <svg viewBox="0 0 24 24">
+            <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 
+            7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
+        </svg>
+
+    `;
+    document.body.appendChild(fullscreen_button);
+  }
+
+  function toggle_fullscreen() {
+    if (!document.fullscreenElement) {
+        document.body.requestFullscreen();
+        document.body.setAttribute("fullscreen",""); 
+    } else {
+        document.exitFullscreen();
+        document.body.removeAttribute("fullscreen"); 
+    }
+  } 
 });
